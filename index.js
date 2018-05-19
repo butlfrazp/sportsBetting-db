824a162329b9d99368d1169419909e9d466ae28c
butlfrazp <paulfbutler2016@gmail.com>
2018-05-19 13:39:48 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const {app} = require('./src/server/server.js');
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
